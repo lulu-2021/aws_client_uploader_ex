623ebbd4bb99a44eb757e7a3945d33d685989b9b
Steve Forkin <steve@netflakes.co>
2018-12-03 21:55:34 +1100

    updated the version number

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AwsClientUploaderEx.MixProject do
   def project do
     [
       app: :aws_client_uploader_ex,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
