70623d8b1ef0f1c129bbea94a881f93673afd2e6
Steve Forkin <netflakes@users.noreply.github.com>
2019-04-29 14:55:05 +1000

    Update aws_client_uploader_ex.ex

--- lib/aws_client_uploader_ex.ex
+++ lib/aws_client_uploader_ex.ex
@@ -5,7 +5,7 @@ defmodule AwsClientUploaderEx do
 
   alias AwsClientUploaderEx.S3Client
 
-  defdelegate signed_download_url(filename), to: S3Client, as: :signed_download_url
+  defdelegate signed_download_url(filename, options \\ []), to: S3Client, as: :signed_download_url
   defdelegate signed_upload_url(filename), to: S3Client, as: :signed_upload_url
   defdelegate list_files(bucket, options \\ []), to: S3Client, as: :list_objects
 end
